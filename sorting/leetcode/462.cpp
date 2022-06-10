5e50508eaebd8ec018ef31e116464d1e3b7654bb
Eunbi Kang <jenabill@naver.com>
2022-06-10 17:30:22 +0900

    Add sorting source file

---
+++ sorting/leetcode/462.cpp
@@ -0,0 +1,40 @@
+// Minimum Moves to Equal Array Elements 2
+
+int sorted[100003];
+void merge(int* nums, int left, int right, int mid){
+    int first = left, second = mid + 1;
+    int i = left;
+    while (first <= mid && second <= right){
+        if (nums[first]  < nums[second]) sorted[i++] = nums[first++];
+        else sorted[i++] = nums[second++];
+    }
+    while (first <= mid){
+        sorted[i++] = nums[first++];
+    }
+    while (second <= right){
+        sorted[i++] = nums[second++];
+    }
+    
+    for (int j = left; j <= right; j++){
+        nums[j] = sorted[j];
+    }
+}
+
+void mergeSort(int* nums, int left, int right){
+    if (left < right){
+        int mid = left + (right - left) / 2;
+        mergeSort(nums, left, mid);
+        mergeSort(nums, mid + 1, right);
+        merge(nums, left, right, mid);
+    }
+}
+int minMoves2(int* nums, int numsSize){
+    mergeSort(nums, 0, numsSize - 1);
+    int median = nums[numsSize / 2];
+    int res = 0;
+    for (int i = 0; i < numsSize; i++){
+        int num = nums[i] - median;
+        res += (num >= 0) ? num : -num;
+    }
+    return res;
+}
