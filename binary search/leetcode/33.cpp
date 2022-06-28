b862e46586852d5640d5987ebf8a34a08b0c2083
Eunbi Kang <jenabill@naver.com>
2022-06-29 00:01:19 +0900

    Update binary search leetcode 33 solution

--- binary search/leetcode/33.cpp
+++ binary search/leetcode/33.cpp
@@ -4,8 +4,8 @@ int search(int* nums, int numsSize, int target){
     while (left <= right){
         int mid = left + (right - left) / 2;
         if (nums[mid] == target) return mid;
-        if (nums[mid] >= nums[left]){
-            if (nums[mid] > target && target >= nums[left]) right = mid - 1;
+        if (nums[left] <= nums[mid]){
+            if (nums[left] <= target && target < nums[mid]) right = mid - 1;
             else left = mid + 1;
         }
         else {
