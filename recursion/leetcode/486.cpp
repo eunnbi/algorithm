aad9a71e345d7e7ed0f4227693b2472d33b14f06
Eunbi Kang <jenabill@naver.com>
2022-06-29 00:28:12 +0900

    Update recursion leetcode 486 solution

--- recursion/leetcode/486.cpp
+++ recursion/leetcode/486.cpp
@@ -1,19 +1,33 @@
-// Predict the Winner
 
 #define max(a, b) (((a) > (b)) ? (a) : (b))
-int predict(int* nums, int left, int right, int turn){
-    if (left == right){
-        return turn * nums[left]; 
-    }
-    
-    // left 선택
-    int a = turn * nums[left] + predict(nums, left + 1, right, -turn);
-    // right 선택
-    int b = turn * nums[right] + predict(nums, left, right - 1, -turn);
-    
-    return turn * max(turn * a, turn * b);
+
+int predict(int* nums, int s, int e, int turn){
+    if (s == e) return turn * nums[s];
+    int a = turn * nums[s] + predict(nums, s + 1, e, -turn); // s 선택
+    int b = turn * nums[e] + predict(nums, s, e - 1, -turn); // e 선택
+    return turn * (max(turn * a, turn * b));
 }
 
 bool PredictTheWinner(int* nums, int numsSize){
     return predict(nums, 0, numsSize - 1, 1) >= 0;
 }
+
+
+/*
+💡 Approach
+- "optimal play" : The player considers all possibilities and makes a choice that results in the biggest score difference.
+    ⇒ "consider all possibilities" : a hint for recursion
+
+- This kind of problems, i.e., two players + optimal play + winner prediction, are usually recursion or dynamic programming problems.
+- "predict(nums, s, e, turn)" : the maxinum score difference that P1 can make using the array nums[s:e]
+    - answer: "predict(nums, s, e, turn)" ≥ 0
+    - turn: 1 (p1) or -1 (p2)
+        - if (turn == 1): add scores to the result
+        - if (turn == -1): subtract scores to the result
+        - res += turn * score
+    - if the player takes the first number, "turns * nums[s] + predict(nums, s + 1, e, -turn)"
+    - if the player takes the last number, "turns * nums[e] + predict(nums, s, e - 1, -turn)"
+    - the condition of termination: "s == e"
+*/
+
+// https://leetcode.com/problems/predict-the-winner/
