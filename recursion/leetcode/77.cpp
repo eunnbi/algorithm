db943d3e5961d34c35f4ba3f1f2b4d67b0619680
Eunbi Kang <jenabill@naver.com>
2022-06-29 00:44:02 +0900

    Update recursion leetcode 77 solution

--- recursion/leetcode/77.cpp
+++ recursion/leetcode/77.cpp
@@ -1,7 +1,8 @@
-// Combinations
+#include <stdlib.h>
 
 int **res, num_comb = 0;
 int comb[30];
+
 int nck(int n, int k){
     if (k == 0 || n == k) return 1;
     return nck(n - 1, k) + nck(n - 1, k - 1);
@@ -35,8 +36,26 @@ int** combine(int n, int k, int* returnSize, int** returnColumnSizes){
     for (int i = 0; i < row; i++){
         res[i] = (int*)malloc(sizeof(int) * k);
     }
+
     num_comb = 0;
     choose(n, k, 0);
     
     return res;
 }
+
+/*
+🚀 Approach
+
+This problem is similar to the "permutation sequence" problem except that
+- We generate strings of length 'k' using 'n' digits
+- The order of digits must be ignored
+    - [1, 2] == [2, 1]
+    - This is little bit tricky.
+    - We will only generate sequences whose elements are in increasing order
+        - Because there is only one ordering where N unique numbers increase, it is guaranteed that all duplicates will be removed
+        - [1, 2, 3] - counted
+        - [3, 2, 1] - not counted
+        - [2, 1, 3] - not counted
+*/
+
+// https://leetcode.com/problems/combinations/
