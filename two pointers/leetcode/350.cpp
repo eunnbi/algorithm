726b2ec87bd6612245c763d0b6a2d6451e5d7526
Eunbi Kang <89760088+eunnbi@users.noreply.github.com>
2022-06-29 19:57:26 +0900

    Update two pointers leetcode 350 solution

--- two pointers/leetcode/350.cpp
+++ two pointers/leetcode/350.cpp
@@ -35,7 +35,7 @@ int* intersect(int* nums1, int nums1Size, int* nums2, int nums2Size, int* return
     int size = (nums1Size < nums2Size) ? nums1Size : nums2Size;
     int * res = (int *)malloc(sizeof(int) * size);
 
-	int i = 0, j = 0; // two pointers
+    int i = 0, j = 0; // two pointers
     int index = 0;
     while (i < nums1Size && j < nums2Size){
         if (nums1[i] == nums2[j]) {
