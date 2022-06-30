4b2bddfa65994a63e9a764cedec586ff60abe679
Eunbi Kang <jenabill@naver.com>
2022-06-30 13:56:59 +0900

    Update stack leetcode 946 solution

--- stack/leetcode/946.cpp
+++ stack/leetcode/946.cpp
@@ -1,13 +1,18 @@
+#include <stack>
+#include <string>
+#include <vector>
 
-// stack and two pointer (pushed, popped)
-int stack[1002];
+using namespace std;
+
+// 1. using stack with array
+int st[1002];
 int top;
 bool validateStackSequences(int* pushed, int pushedSize, int* popped, int poppedSize){
     top = 0;
     int j = 0; // index of popped
     for (int  i = 0; i < pushedSize; i++){
-        stack[top++] = pushed[i];
-        while (top > 0 && stack[top - 1] == popped[j]){
+        st[top++] = pushed[i];
+        while (top > 0 && st[top - 1] == popped[j]){
             top--;
             j++;
         }
@@ -15,4 +20,22 @@ bool validateStackSequences(int* pushed, int pushedSize, int* popped, int popped
     return j == poppedSize;
 }
 
+
+// 2. STL stack
+bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+    stack<int> st;
+    int j = 0;
+    int pushedSize = pushed.size();
+    for (int i = 0; i < pushedSize; i++){
+        st.push(pushed[i]);
+        while (!st.empty() && st.top() == popped[j]){
+            st.pop();
+            j++;
+        }
+    }
+    return j == popped.size();
+}
+
+
+// 👉 stack and two pointer (pushed, popped)
 // https://leetcode.com/problems/validate-stack-sequences/
