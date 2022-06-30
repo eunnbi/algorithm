f8d3737d6fc0e4b126bb1beecf89c73b7ebb8bc2
Eunbi Kang <jenabill@naver.com>
2022-06-30 13:52:42 +0900

    Update stack leetcode 856 solution

--- stack/leetcode/856.cpp
+++ stack/leetcode/856.cpp
@@ -1,23 +1,58 @@
 #include <string.h>
+#include <stack>
+#include <string>
 
-int stack[51], top;
+using namespace std;
+
+// 1. using stack with array
+int st[51], top;
 int scoreOfParentheses(char * s){
     top = 0;
     int len = strlen(s);
     int sum;
     for (int i = 0; i < len; i++){
-        if (s[i] == '(') stack[top++] = -1;
-        else if (stack[top - 1] == -1) stack[top - 1] = 1;
+        if (s[i] == '(') st[top++] = -1;
+        else if (st[top - 1] == -1) st[top - 1] = 1;
         else {
             sum = 0;
-            while (stack[--top] != -1) {
-                sum += stack[top];
+            while (st[--top] != -1) {
+                sum += st[top];
             }
-            stack[top++] = sum * 2;
+            st[top++] = sum * 2;
         }
     }
     sum = 0;
-    while (--top >= 0) sum += stack[top];
+    while (--top >= 0) sum += st[top];
+    return sum;
+}
+
+
+// 2. STL stack
+int scoreOfParentheses(string s) {
+    stack<int> st;
+    int len = s.length();
+    int sum = 0;
+    for (int i = 0; i < len; i++){
+        if (s[i] == '(') st.push(-1);
+        else if (st.top() == -1) {
+            st.pop();
+            st.push(1);
+        }
+        else {
+            sum = 0;
+            while (st.top() != -1){
+                sum += st.top();
+                st.pop();
+            }
+            st.pop();
+            st.push(sum * 2);
+        }
+    }
+    sum = 0;
+    while (!st.empty()) {
+        sum += st.top();
+        st.pop();
+    }
     return sum;
 }
 
