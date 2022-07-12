e3243221c83e2f736c12804928029c7935c03b6e
Eunbi Kang <jenabill@naver.com>
2022-07-12 21:11:59 +0900

    Update recursion BOJ 1629

--- recursion/BOJ/1629.cpp
+++ recursion/BOJ/1629.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 long long POW(long long a, long long b, long long m){
     if (b == 1) return a % m;
-    long long val = POW(a, b / 2, m);
+    long long val = POW(a, b / 2, m); 
     val = val * val % m;
     if (b % 2 == 0) return val;
     return val * a % m;
@@ -20,4 +20,5 @@ int main(void){
 
 // a^n * a^n = a^2n
 // 1승을 계산할 수 있다. k승을 계산하면 2k승과 2k + 1을 O(1)에 계산할 수 있다.
+// 마지막 한 순간에만 나머지 연산을 할 필요 없음. 중간중간에 나머지 연산해도 ok
 // https://www.acmicpc.net/problem/1629
