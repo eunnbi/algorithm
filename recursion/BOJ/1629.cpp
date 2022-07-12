9571556724f74f397604f6613ad2d541c6288062
Eunbi Kang <jenabill@naver.com>
2022-07-12 21:09:52 +0900

    Add: recursion BOJ 1629

---
+++ recursion/BOJ/1629.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+long long POW(long long a, long long b, long long m){
+    if (b == 1) return a % m;
+    long long val = POW(a, b / 2, m);
+    val = val * val % m;
+    if (b % 2 == 0) return val;
+    return val * a % m;
+}
+
+
+int main(void){
+    long long a, b, c;
+    cin >> a >> b >> c;
+    cout << POW(a, b, c);
+    return 0;
+}
+
+// a^n * a^n = a^2n
+// 1승을 계산할 수 있다. k승을 계산하면 2k승과 2k + 1을 O(1)에 계산할 수 있다.
+// https://www.acmicpc.net/problem/1629
