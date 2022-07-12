ae97be77d60697746222b559cc41b9b5833c3686
Eunbi Kang <jenabill@naver.com>
2022-07-12 21:09:35 +0900

    Add: recursion BOJ 1074

---
+++ recursion/BOJ/1074.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int sol(int n, int r, int c){
+    if (n == 0) return 0;
+    int half = 1 << (n - 1);
+    if (r < half && c < half) return sol(n - 1, r, c);
+    if (r < half && c >= half) return sol(n - 1, r, c - half) + half * half;
+    if (r >= half && c < half) return sol(n - 1, r - half, c) + 2 * half * half;
+    return sol(n - 1, r - half, c - half) + 3 * half * half;
+}
+
+int main(void){
+    int n, r, c;
+    cin >> n >> r >> c;
+    cout << sol(n, r, c);
+    return 0;
+}
