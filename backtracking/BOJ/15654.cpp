6e58350c4135af85d47098914a47e705be260951
Eunbi Kang <jenabill@naver.com>
2022-07-15 21:03:15 +0900

    Add: backtracking BOJ 15654

---
+++ backtracking/BOJ/15654.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+int nums[10];
+int used[10];
+int res[10];
+
+void sol(int i){
+    if (i == M){
+        for (int j = 0; j < M; j++){
+            printf("%d ", nums[res[j]]);
+        }
+        printf("\n");
+        return;
+    }
+
+    for (int j = 0; j < N; j++){
+        if (used[j]) continue;
+        used[j] = 1;
+        res[i] = j;
+        sol(i + 1);
+        used[j] = 0;
+    }
+}
+
+int main(void){
+    cin >> N >> M;
+    for (int i = 0; i < N; i++){
+        cin >> nums[i];
+    }
+    sort(nums, nums + N);
+    sol(0);
+    return 0;
+}
