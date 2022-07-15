9fb5f2a4460f80e16967d5cb95a9b128d2888e9c
Eunbi Kang <jenabill@naver.com>
2022-07-15 21:01:10 +0900

    Add: backtracking BOJ 15651

---
+++ backtracking/BOJ/15651.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+int res[10];
+
+void sol(int i){
+    if (i == M){
+        for (int j = 0; j < M; j++){
+            printf("%d ", res[j]);
+        }
+        printf("\n");
+        return;
+    }
+    for (int j = 1; j <= N; j++){
+        res[i] = j;
+        sol(i + 1);
+    }
+}
+
+int main(void){
+    cin >> N >> M;
+    sol(0);
+    return 0;
+}
+
+// https://www.acmicpc.net/problem/15651
