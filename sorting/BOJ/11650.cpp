849cf5bab3d981e0b2b5a09f7c7384566e52457e
Eunbi Kang <jenabill@naver.com>
2022-07-29 07:22:53 +0900

    Add: sorting BOJ 11650

---
+++ sorting/BOJ/11650.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+vector<pair<int, int>> coord;
+
+bool cmp(const pair<int, int>& a, const pair<int, int>& b) {
+    if (a.first != b.first) return a.first < b.first;
+    return a.second < b.second;
+}
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N;
+    int x, y;
+    for (int i = 0; i < N; i++) {
+        cin >> x >> y;
+        coord.push_back({ x, y });
+    }
+    sort(coord.begin(), coord.end(), cmp);
+    for (int i = 0; i < N; i++) {
+        cout << coord[i].first << ' ' << coord[i].second << '\n';
+    }
+    return 0;
+}
