ffb4885e1ddbde7f2be9957649968e2c18ed258d
Eunbi Kang <jenabill@naver.com>
2022-08-01 16:54:45 +0900

    Add: dp BOJ 11727

---
+++ dynamic programming/BOJ/11727.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+int dp[1001];
+
+int main(void) {
+    cin >> N;
+    dp[1] = 1;
+    dp[2] = 3;
+    for (int i = 3; i <= N; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+    }
+    printf("%d\n", dp[N]);
+    return 0;
+}
