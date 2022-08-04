7cb692ca0472ab76dcc8e65ae7ba7600d374a17c
Eunbi Kang <jenabill@naver.com>
2022-08-05 04:48:10 +0900

    Add: dp BOJ 11057

---
+++ dynamic programming/BOJ/11057.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define MOD 10007
+using namespace std;
+
+int N;
+long long dp[1001][10]; // 
+
+
+int main(void) {
+    cin >> N;
+    for (int i = 0; i < 10; i++) {
+        dp[1][i] = 1;
+    }
+    for (int i = 2; i <= N; i++) {
+        for (int j = 0; j < 10; j++) {
+            for (int k = 0; k <= j; k++) {
+                dp[i][j] += dp[i - 1][k] % MOD;
+            }
+        }
+    }
+    long long res = 0;
+    for (int i = 0; i < 10; i++) {
+        res += dp[N][i];
+    }
+    cout << res % MOD;
+    return 0;
+}
