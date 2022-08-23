924f23cd08c6689177b225d70c3eb8d2b68354a1
Eunbi Kang <jenabill@naver.com>
2022-08-23 22:31:02 +0900

    Add: binary search BOJ 18870

---
+++ binary search/BOJ/18870.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+int x[1000001];
+vector<int> uni; // 중복 제거
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> x[i];
+        uni.push_back(x[i]);
+    }
+
+    sort(uni.begin(), uni.end());
+    uni.erase(unique(uni.begin(), uni.end()), uni.end()); // 중복 제거 🔥
+    for (int i = 0; i < N; i++) {
+        cout << lower_bound(uni.begin(), uni.end(), x[i]) - uni.begin() << ' ';
+    }
+    return 0;
+}
+
+// https://www.acmicpc.net/problem/18870
