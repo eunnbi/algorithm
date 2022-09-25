7b4815441f4f70ebd8cfb0f912ebe4973cba3bff
Eunbi Kang <jenabill@naver.com>
2022-09-25 14:43:46 +0900

    update dp BOJ 1463

--- dynamic programming/BOJ/1463.cpp
+++ dynamic programming/BOJ/1463.cpp
@@ -1,28 +1,26 @@
 #include <bits/stdc++.h>
-#define MAX 1000001
 using namespace std;
 
 int N;
-int dp[MAX]; // dp[i] : i를 1로 만드는데 사용되는 연산의 개수의 최솟값
+int dp[1000001]; // dp[i] : i를 1로 만드는데 사용되는 연산의 개수의 최솟값
 
 int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
 	cin >> N;
-	dp[0] = dp[1] = 0; // base case
+	dp[1] = 0;
 	for (int i = 2; i <= N; i++) {
-		int num = MAX;
+		int num = dp[i - 1];
 		if (!(i % 3)) {
-			int tmp = i / 3;
-			if (num > dp[tmp] + 1) num = dp[tmp] + 1;
+			num = min(num, dp[i / 3]);
 		}
 		if (!(i % 2)) {
-			int tmp = i / 2;
-			if (num > dp[tmp] + 1) num = dp[tmp] + 1;
-
+			num = min(num, dp[i / 2]);
 		}
-		if (num > dp[i - 1] + 1) num = dp[i - 1] + 1;
-		dp[i] = num;
+		dp[i] = num + 1;
 	}
-	printf("%d\n", dp[N]); // answer case
+	cout << dp[N];
 	return 0;
 }
 
