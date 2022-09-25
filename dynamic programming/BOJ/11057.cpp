5c8ea38761612657ce8ae06bb3663c8695bac149
Eunbi Kang <jenabill@naver.com>
2022-09-25 16:18:11 +0900

    update dp BOJ 11057

--- dynamic programming/BOJ/11057.cpp
+++ dynamic programming/BOJ/11057.cpp
@@ -7,21 +7,27 @@ long long dp[1001][10]; //
 
 
 int main(void) {
-    cin >> N;
-    for (int i = 0; i < 10; i++) {
-        dp[1][i] = 1;
-    }
-    for (int i = 2; i <= N; i++) {
-        for (int j = 0; j < 10; j++) {
-            for (int k = 0; k <= j; k++) {
-                dp[i][j] += dp[i - 1][k] % MOD;
-            }
-        }
-    }
-    long long res = 0;
-    for (int i = 0; i < 10; i++) {
-        res += dp[N][i];
-    }
-    cout << res % MOD;
-    return 0;
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N;
+
+	for (int i = 0; i < 10; i++) {
+		dp[1][i] = 1;
+	}
+
+	for (int i = 2; i <= N; i++) {
+		for (int j = 0; j < 10; j++) {
+			for (int k = 0; k <= j; k++) {
+				dp[i][j] = (dp[i][j] + dp[i - 1][k]) % MOD;
+			}
+		}
+	}
+
+	long long res = 0;
+	for (int i = 0; i < 10; i++) {
+		res += dp[N][i] % MOD;
+	}
+	cout << res % MOD;
+	return 0;
 }
