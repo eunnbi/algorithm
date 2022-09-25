82a931ab4569db5b4010c4ecc01ac4d60a56ad0d
Eunbi Kang <jenabill@naver.com>
2022-09-25 16:11:09 +0900

    update dp BOJ 10844

--- dynamic programming/BOJ/10844.cpp
+++ dynamic programming/BOJ/10844.cpp
@@ -6,22 +6,25 @@ int N;
 long long dp[101][10];
 
 int main(void) {
-    cin >> N;
-    dp[1][0] = 0;
-    for (int i = 1; i < 10; i++) {
-        dp[1][i] = 1;
-    }
-    for (int i = 2; i <= N; i++) {
-        for (int j = 0; j <= 9; j++) {
-            if (j == 0) dp[i][j] = dp[i - 1][j + 1] % MOD;
-            else if (j == 9) dp[i][j] = dp[i - 1][j - 1] % MOD;
-            else dp[i][j] = (dp[i - 1][j + 1] + dp[i - 1][j - 1]) % MOD;
-        }
-    }
-    long long res = 0;
-    for (int i = 0; i <= 9; i++) {
-        res += dp[N][i];
-    }
-    cout << res % MOD;
-    return 0;
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N;
+	dp[1][0] = 0;
+	for (int i = 1; i < 10; i++) {
+		dp[1][i] = 1;
+	}
+
+	for (int i = 2; i <= N; i++) {
+		for (int j = 0; j < 10; j++) {
+			dp[i][j] = ((j == 0 ? 0 : dp[i - 1][j - 1]) + (j == 9 ? 0 : dp[i - 1][j + 1])) % MOD;
+		}
+	}
+
+	long long res = 0;
+	for (int i = 0; i < 10; i++) {
+		res += dp[N][i] % MOD;
+	}
+	cout << res % MOD;
+	return 0;
 }
