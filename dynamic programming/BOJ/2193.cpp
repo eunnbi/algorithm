8fbc622825d7abe6f81f39640b2e8b2669c83f92
Eunbi Kang <jenabill@naver.com>
2022-09-25 15:10:30 +0900

    update dp BOJ 2193

--- dynamic programming/BOJ/2193.cpp
+++ dynamic programming/BOJ/2193.cpp
@@ -6,17 +6,19 @@ int N;
 // dp[i][1] : 1로 끝나는 i자리 이친수의 개수
 long long dp[91][2]; // integer overflow 주의!!
 int main(void) {
-    cin >> N;
-    dp[1][0] = 0;
-    dp[1][1] = 1;
-    dp[2][0] = 1;
-    dp[2][1] = 0;
-    for (int i = 3; i <= N; i++) {
-        dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
-        dp[i][1] = dp[i - 1][0];
-    }
-    cout << dp[N][0] + dp[N][1];
-    return 0;
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N;
+	dp[1][0] = 0;
+	dp[1][1] = 1;
+	for (int i = 2; i <= N; i++) {
+		dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
+		dp[i][1] = dp[i - 1][0];
+	}
+
+	cout << dp[N][0] + dp[N][1];
+	return 0;
 }
 
 // https://www.acmicpc.net/problem/2193
