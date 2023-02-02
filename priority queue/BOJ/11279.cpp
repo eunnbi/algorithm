a17e0bf2c81c0092c0ff5d6fa3e06d19fc03b405
Eunbi <jenabill@naver.com>
2023-02-02 17:38:04 +0900

    add priority queue BOJ 11279 problem solution

---
+++ priority queue/BOJ/11279.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    // ⚠️ 아래 두줄 지우면 시간초과 발생
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n, x;
+    priority_queue<int> pq;
+    
+    cin >> n;
+    while (n--) {
+        cin >> x;
+        if (x == 0) {
+            if (pq.empty()) cout << 0 << '\n';
+            else {
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+        }
+        else pq.push(x);
+    }
+    return 0;
+}
